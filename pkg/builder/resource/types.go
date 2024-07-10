55ec0e5986462c60870f44a87af7475a67b3b76c
Wim Henderickx <wim.henderickx@gmail.com>
2024-07-10 08:48:35 +0200

    updated object

--- pkg/builder/resource/types.go
+++ pkg/builder/resource/types.go
@@ -57,29 +57,7 @@ type Object interface {
 }
 
 type InternalObject interface {
-	// Object allows the apiserver libraries to operate on the Object
-	runtime.Object
-
-	// GetObjectMeta returns the object meta reference.
-	GetObjectMeta() *metav1.ObjectMeta
-
-	// Scoper is used to qualify the resource as either namespace scoped or non-namespace scoped.
-	rest.Scoper
-
-	// New returns a new instance of the resource -- e.g. &v1.Deployment{}
-	New() runtime.Object
-
-	// NewList return a new list instance of the resource -- e.g. &v1.DeploymentList{}
-	NewList() runtime.Object
-
-	// GetGroupVersionResource returns the GroupVersionResource for this resource.  The resource should
-	// be the all lowercase and pluralized kind.s
-	GetGroupVersionResource() schema.GroupVersionResource
-
-	// IsStorageVersion returns true if the object is also the internal version -- i.e. is the type defined
-	// for the API group an alias to this object.
-	// If false, the resource is expected to implement MultiVersionObject interface.
-	IsStorageVersion() bool
+	Object
 
 	// GetSingularName
 	GetSingularName() string
