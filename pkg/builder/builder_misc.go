f671162702c114e2159ccb4d0f6c505691620771
Wim Henderickx <wim.henderickx@gmail.com>
2026-02-19 09:18:03 +0100

    added debug

--- pkg/builder/builder_misc.go
+++ pkg/builder/builder_misc.go
@@ -2,7 +2,8 @@ package builder
 
 import (
 	"fmt"
-	
+	"strings"
+
 	"github.com/henderiw/apiserver-builder/pkg/apiserver"
 	"github.com/henderiw/apiserver-builder/pkg/cmd/apiserverbuilder/options"
 	openapinamer "k8s.io/apiserver/pkg/endpoints/openapi"
@@ -15,26 +16,41 @@ import (
 func (r *Server) WithOpenAPIDefinitions(
 	name, version string,
 	defs openapicommon.GetOpenAPIDefinitions) *Server {
+	
+	namer := openapinamer.NewDefinitionNamer(apiserver.Scheme, scheme.Scheme)
+
+    // Convert github.com/foo/bar.Type → github.com.foo.bar.Type
+    // so $ref strings use dots (no ~1 encoding) and match map keys exactly.
+    getDefinitionName := func(n string) (string, spec.Extensions) {
+        if strings.HasPrefix(n, "github.com/") {
+            return strings.ReplaceAll(n, "/", "."), nil
+        }
+        return namer.GetDefinitionName(n)
+    }
+
 	mergedDefs := func(ref openapicommon.ReferenceCallback) map[string]openapicommon.OpenAPIDefinition {
-		result := apiextensionsopenapi.GetOpenAPIDefinitions(ref)
-		for k, v := range defs(ref) {
-			result[k] = v
-		}
-		return result
-	}
+        result := apiextensionsopenapi.GetOpenAPIDefinitions(ref)
+        for k, v := range defs(ref) {
+            if strings.HasPrefix(k, "github.com/") {
+                result[strings.ReplaceAll(k, "/", ".")] = v
+            } else {
+                result[k] = v
+            }
+        }
+        return result
+    }
 	
 	// DEBUG: check what the namer returns for our types
-    namer := openapinamer.NewDefinitionNamer(apiserver.Scheme, scheme.Scheme)
     defName, _ := namer.GetDefinitionName("github.com/sdcio/config-server/apis/config/v1alpha1.Config")
     fmt.Printf("DEBUG: namer Config = %q\n", defName)
     defName2, _ := namer.GetDefinitionName("github.com/sdcio/config-server/apis/config/v1alpha1.DeviationSpec")
     fmt.Printf("DEBUG: namer DeviationSpec = %q\n", defName2)
 		
 	options.RecommendedConfigFns = append(options.RecommendedConfigFns, func(config *server.RecommendedConfig) *server.RecommendedConfig {
-		config.OpenAPIConfig = server.DefaultOpenAPIConfig(mergedDefs, openapinamer.NewDefinitionNamer(apiserver.Scheme, scheme.Scheme))
+		config.OpenAPIConfig = server.DefaultOpenAPIConfig(mergedDefs, namer)
 		config.OpenAPIConfig.Info.Title = name
 		config.OpenAPIConfig.Info.Version = version
-		config.OpenAPIV3Config = server.DefaultOpenAPIV3Config(mergedDefs, openapinamer.NewDefinitionNamer(apiserver.Scheme, scheme.Scheme))
+		config.OpenAPIV3Config = server.DefaultOpenAPIV3Config(mergedDefs, namer)
 		config.OpenAPIV3Config.Info.Title = name
 		config.OpenAPIV3Config.Info.Version = version
 		return config
